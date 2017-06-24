d5550827ec3f7d69b3d8a82659a872c59a72f90a
hugo <hdf1986.6700@gmail.com>
2017-06-24 19:27:45 -0300

    Did you know that :watermelon: emojis work on github?

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,10 +1,10 @@
 class ChatController < ApplicationController
   def index
     ActionCable.server.broadcast(
-  "chat_room",
-  sent_by: 'Paul',
-  body: 'This is a cool chat app.'
-)
+      "chat_room",
+      sent_by: 'Paul',
+      body: 'This is a cool chat app.'
+    )
     head :ok
   end
 end
