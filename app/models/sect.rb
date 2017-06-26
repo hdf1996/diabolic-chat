8b019d21c653b8999247b2a368895fedd434d987
hugo <hdf1986.6700@gmail.com>
2017-06-26 02:34:33 -0300

    Added specs for subscription to sect

--- app/models/sect.rb
+++ app/models/sect.rb
@@ -3,4 +3,9 @@ class Sect < ApplicationRecord
   has_many :users, through: :sect_subscriptions
 
   validates :name, presence: true
+  validate :limit_reached
+
+  def limit_reached
+    errors.add('users.limit_reached') if users.count >= max_size
+  end
 end
--- app/models/sect_subscription.rb
+++ app/models/sect_subscription.rb
@@ -3,6 +3,10 @@ class SectSubscription < ApplicationRecord
   belongs_to :user
 
   validates :role, presence: true, inclusion: { in: 0..1 }
+  validates :user_id, uniqueness: { scope: :sect_id }
+  validate :sect_limit_reached
+
+  delegate :limit_reached, to: :sect, prefix: true
 
   enum roles: { user: 0, admin: 1 }
 end
