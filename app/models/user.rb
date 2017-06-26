5cdae88966082c45d8c8bc5a77c85bc771e68e37
hugo <hdf1986.6700@gmail.com>
2017-06-26 01:16:33 -0300

    Added authentication endpoints

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def generate_access_token
+    self.access_token = SecureRandom.hex(16)
+  end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
       it 'returns the user' do
         post :create, params: { user: user_params }
-        expect(JSON.parse(response.body).keys).to eq %w[id email created_at]
+        expect(JSON.parse(response.body).keys).to eq %w[id email]
       end
     end
 
@@ -42,10 +42,35 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     context 'with token' do
       let!(:user) { create(:user) }
 
-      before { get :me }
+      before { get :me, params: { access_token: user.access_token } }
 
       it 'returns ok' do
         expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns the current_user' do
+        expect(JSON.parse(response.body).symbolize_keys).to eq(UserSerializer.new(user).as_json)
+      end
+    end
+  end
+
+  describe '#authenticate' do
+    context 'with credentials' do
+      let!(:user) do
+        create(:user, password: 'some_password',
+                      password_confirmation: 'some_password')
+      end
+
+      before do
+        post :authenticate, params: { user: { email: user.email, password: 'some_password' } }
+      end
+
+      it 'returns ok' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns the access token' do
+        expect(JSON.parse(response.body)).to eq('access_token' => user.access_token)
       end
     end
   end
