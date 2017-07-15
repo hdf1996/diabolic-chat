c2f7f3993f2086b61fec8abc18e26f068139dc82
hugo <hdf1986.6700@gmail.com>
2017-07-15 14:13:44 -0300

    added login <3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users, controllers: {
+    registrations: :registrations
+  }
   root 'static#index'
 
   authenticate :user do
