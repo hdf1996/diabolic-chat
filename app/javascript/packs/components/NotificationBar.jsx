41a295191c1513381d6bb06d129906368a00d060
tomi <tomel95@hotmail.com>
2017-07-15 23:29:35 -0300

    want notifications?

--- app/javascript/packs/components/NotificationBar.jsx
+++ app/javascript/packs/components/NotificationBar.jsx
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const NotificationContainer = styled.div`
+  position: fixed;
+  bottom: 120px;
+  z-index: 999;
+  left: calc(50% - 175px + 125px);
+  width: 100%;
+  max-width: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 50px;
+  color: white;
+`;
+
 class NotificationBar extends Component {
   constructor (props) {
     super()
@@ -24,20 +38,39 @@ class NotificationBar extends Component {
     }
   }
 
+  byeNotification = () =>{
+    this.setState({showDialog: false})
+  }
+
   ableToAsk() {
     return ("Notification" in window) && Notification.permission !== 'denied';
   }
 
   render () {
     return (
-      <div onClick={this.askPermission}>
-        {(function () {
-            if (this.state.showDialog) {
-              return 'Do you want notifications?';
-            }
-          }).bind(this)()
-        }
-      </div>
+      <NotificationContainer>
+        <div>
+          {(function () {
+              if (this.state.showDialog) {
+                return (
+                  <article>
+
+                    <h2>Do you want notifications?</h2>
+                    <button onClick={this.askPermission}>
+                      Yes
+                    </button>
+                    <button onClick={this.byeNotification}>
+                      No
+                    </button>
+
+                  </article>
+
+                );
+              }
+            }).bind(this)()
+          }
+        </div>
+      </NotificationContainer>
     );
   }
 
