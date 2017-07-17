78dcd94a359cf57fca182345631d01adeb422063
hugo <hdf1986.6700@gmail.com>
2017-07-17 16:40:07 -0300

    Fixed bad casting

--- app/controllers/api/v1/sects_controller.rb
+++ app/controllers/api/v1/sects_controller.rb
@@ -14,7 +14,7 @@ module Api
       end
 
       def messages
-        render json: Sect.find(params[:id]).messages.order(id: :desc).limit(10).to_a.inverse
+        render json: Sect.find(params[:id]).messages.order(id: :desc).limit(10).to_a.reverse
       end
 
       def subscribe
