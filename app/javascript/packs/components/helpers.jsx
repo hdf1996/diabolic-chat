36390e09a81e6931bb18c79d5ea2b28367f5d7ec
hugo <hdf1986.6700@gmail.com>
2017-07-18 00:43:43 -0300

    Added rick roll

--- app/javascript/packs/components/helpers.jsx
+++ app/javascript/packs/components/helpers.jsx
@@ -35,7 +35,7 @@ window.audio = (url) => {
 }
 
 window.rickrollit = () => {
-  
+  $("*").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe> ');
 }
 
 window.scream = () => {
